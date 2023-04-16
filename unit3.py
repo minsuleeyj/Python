f36b09301e4344faff0a55d2bb315d9d82ea2f1f
minsuleeyj <minsu3309@skuniv.ac.kr>
2023-04-16 23:19:48 +0900

    add text file [unit3.txt]

---
+++ unit3.py
@@ -0,0 +1,10 @@
+family=7
+room=3
+
+if room>family/3 :
+    print('부부끼리 자고 민수는 혼자 자거라')
+if room<family/3 :
+    print('몇명은 거실에서 자자')
+if 3<5:
+    print('민수는 귀엽다')
+#i키가 망가져서 이번주는 찍먹만 해봅니당 키보드 배달 오면 제대로 해볼게용 하필 if문 할 때 i가 안 눌려서;;;
